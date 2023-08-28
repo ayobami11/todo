22a7ce95e4fc754fe8566c3a12dc0b4df8aacb8d
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2023-08-28 15:30:20 +0100

    Modified endpoint logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,9 +52,9 @@ export const authOptions: NextAuthOptions = {
                         return null;
                     }
 
-                    const endpoint = process.env.NODE_ENV === 'production' ?
-                        'https://todo-app-ayo.vercel.app/api/auth/login' :
-                        'http://localhost:3000/api/auth/login';
+                    const endpoint = process.env.NODE_ENV === 'development' ?
+                        'http://localhost:3000/api/auth/login' :
+                        'https://todo-app-ayo.vercel.app/api/auth/login';
 
                     const response = await fetch(endpoint, {
                         method: 'POST',
